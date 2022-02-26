ca30d6f98474d07cb0e6b248fc2274318db3949d
bignyokki <shiro5989@yahoo.co.jp>
2022-02-26 16:36:40 +0900

    他のユーザーの削除機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   get 'users/index'
-  devise_for :users
+  
+  devise_for :users, :controllers => {
+    # オーバーライドした'users/registrations_controller'をルーティングする
+    :registrations => 'users/registrations'
+  }
+  # devise_scopeで新しくルーティングを追加する
+  devise_scope :user do
+    get 'users/:id/edit' => 'users/registrations#edit', as: :edit_other_user_registration
+    match 'users/:id', to: 'users/registrations#update', via: [:patch, :put], as: :other_user_registration
+    delete 'users/:id', to: 'users/registrations#destroy', as: :destroy_other_user_registration
+  end
+
   root to: "menus#index"
   get 'menus/index'
 end
