d9b9a171fce7527d2ffbbe475d3d914076c18641
bignyokki <shiro5989@yahoo.co.jp>
2022-05-07 13:55:57 +0900

    承認編集画面のデザイン

--- app/controllers/approvals_controller.rb
+++ app/controllers/approvals_controller.rb
@@ -13,8 +13,8 @@ class ApprovalsController < ApplicationController
   def update
     @quotation = Quotation.find(params[:id])
     # 押されたボタンによって承認状況を更新（承認:1, 差し戻し:2)
-    @quotation.updata(approval: 1) if params[:approval_btn]
-    @quotation.updata(approval: 2) if params[:back_btn]
+    @quotation.update(approval: 1) if params[:approval_btn]
+    @quotation.update(approval: 2) if params[:back_btn]
     if @quotation.update(quotation_params)
       redirect_to quotations_path
     else
@@ -25,10 +25,10 @@ class ApprovalsController < ApplicationController
   private
 
   def quotation_params
-    params.require(:quotation)
-          .permit(:client_id, :title, :charge, :delivery_date, :expiration_date,
-                  :delivery_place, :business_terms, :total_price, :remarks)
-          .merge(appro_user_id: current_user.id)
+      params.require(:quotation)
+            .permit(:client_id, :title, :charge, :delivery_date, :expiration_date,
+                    :delivery_place, :business_terms, :total_price, :remarks)
+            .merge(appro_user_id: current_user.id)
   end
 
 end
