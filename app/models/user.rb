1a23e4fb9f80990cd181d4a3a73f5876b9c27202
bignyokki <shiro5989@yahoo.co.jp>
2022-05-20 00:05:11 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
 
-  validates :employee_number, presence: true, uniqueness: true
+  validates :employee_number, presence: true, uniqueness: true, format: { with: /\A[0-9]{4}\z/ } #数字4桁のみ
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+{8,16}\z/i } #英字、数字を含む8~16文字
+  validates :name, presence: true
+  validates :admin_id, presence: true
 
   def email_required?
     false
