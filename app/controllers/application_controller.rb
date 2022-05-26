91caae97a633cf6d12b30a81bdddd8d4fd52f46a
bignyokki <shiro5989@yahoo.co.jp>
2022-05-27 01:34:47 +0900

    ログインしなければ使用不可に

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user! 
 
   rescue_from CanCan::AccessDenied do |_exception|
     redirect_to root_path, alert: '画面を閲覧する権限がありません。'
