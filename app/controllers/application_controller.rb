f10bddfb57fb579c7a163aa38064da8b5f06d65f
bignyokki <shiro5989@yahoo.co.jp>
2022-06-12 04:52:33 +0900

    Basic認証を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
+  # Basic認証
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # ログインしていなければログイン画面に遷移する
   before_action :authenticate_user! 
 
   rescue_from CanCan::AccessDenied do |_exception|
@@ -8,6 +11,13 @@ class ApplicationController < ActionController::Base
 
 
   private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      # 環境変数を読み込み
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:employee_number, :name, :admin_id])
