3cbb675ab1bb48d748990b143d6057cfb359c9cb
bignyokki <shiro5989@yahoo.co.jp>
2022-06-12 04:52:49 +0900

    結合テスト：見積新規作成

--- spec/factories/drawings.rb
+++ spec/factories/drawings.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :drawing do
     figure_number     { Faker::Lorem.characters(number: 10) }
+    product_name      { Faker::Lorem.word }
+    material          { Faker::Lorem.word }
     surface_treatment { Faker::Lorem.word }
     quantity          { Faker::Number.within(range: 1..1000) }
     price             { Faker::Number.within(range: 1..100000) }
--- spec/factories/quotations.rb
+++ spec/factories/quotations.rb
@@ -1,6 +1,14 @@
 FactoryBot.define do
   factory :quotation do
+    transient do
+      person { Gimei.name }
+    end
     title { Faker::Lorem.word }
+    charge { person.first.kanji }
+    delivery_date { "#{Faker::Number.within(range: 1..30)}日"}
+    expiration_date { '1ヶ月' }
+    delivery_place { '御社' }
+    business_terms { '従来通り' }
     total_price { Faker::Number.within(range: 1..100000) }
     association :client
   end
