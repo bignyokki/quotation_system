6de28a6d2fee15119f593398b07d440411a48b95
bignyokki <shiro5989@yahoo.co.jp>
2022-06-19 08:36:57 +0900

    顧客formのフローティングラベル実装

--- app/javascript/floating_label.js
+++ app/javascript/floating_label.js
@@ -1,10 +1,10 @@
 $(document).on('turbolinks:load', function() {
 
-  $('input').on('focusin', function() {
+  $('input, textarea').on('focusin', function() {
     $(this).parent().find('.field__label').addClass('active');
   });
   
-  $('input').on('focusout', function() {
+  $('input, textarea').on('focusout', function() {
     if (!this.value) {
       $(this).parent().find('.field__label').removeClass('active');
     }
