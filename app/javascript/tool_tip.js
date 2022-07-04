35b5191ea703c327fde5753eebffac3da6e4603f
bignyokki <shiro5989@yahoo.co.jp>
2022-07-05 01:47:36 +0900

    ツールチップ実装

--- app/javascript/tool_tip.js
+++ app/javascript/tool_tip.js
@@ -1,12 +1,13 @@
 //インプットエリアをループ
-$('form .form').each(function() {
-  var formVal = $(this)
+$('form .field').each(function() {
+  var formVal = $(this);
   //フォーカスされたらツールチップを表示
   formVal.find('.tool_tiper').focus(function() {
-    formVal.find('.tool_tip_msg').fadeIn(200)
-  }) 
-})
+    console.log("イベント発火");
+    formVal.find('.tool_tip_msg').fadeIn(200);
+  });
+});
 //フォーカスが外れたらツールチップを非表示
 $('.tool_tiper').blur(function() {
-  $('form').find('.tool_tip_msg').fadeOut(200)
-})
+  $('form').find('.tool_tip_msg').fadeOut(200);
+});
